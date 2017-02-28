9990d326a43739fe466834ba2d1d70605bd737b5
Chris Jungerius <cjungerius@olbico.nl>
2017-02-28 15:01:17 +0100

    Added alternative value with constraints

--- vsts-variable-set/vsts-variable-set.ps1
+++ vsts-variable-set/vsts-variable-set.ps1
@@ -8,20 +8,32 @@ param(
     [Parameter(Mandatory=$false)]
     $IfVariable = "",
     [Parameter(Mandatory=$false)]
-    $IfValueRegex = ""
+    $IfValueRegex = "",
+    [Parameter(Mandatory=$false)]
+    $AlternativeValue = "",
+    [Parameter(Mandatory=$false)]
+    $applyConstraint = "",
+    [Parameter(Mandatory=$false)]
+    $applyAlternativeValue = ""
 )
 
 Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
 Write-Verbose "Parameter Values"
 $PSBoundParameters.Keys | %{ Write-Verbose "$_ = $($PSBoundParameters[$_])" }
 
-if ($IfVariable -ne "") 
+if ($applyConstraint.ToUpper() -eq "TRUE") 
 {
     Write-Verbose "Checking if $IfVariable is matched by $IfValueRegex"
     if (-Not($IfVariable -match $IfValueRegex))
     {
-        Write-Verbose "Value not set. Could not match if constraint."
-        exit
+        if ($applyAlternativeValue -eq "FALSE")
+        {
+            Write-Verbose "Value not set. Could not match if constraint."
+            exit
+        } else {            
+            Write-Verbose "Using alternative value. Could not match if constraint."
+            $Value = $AlternativeValue;
+        }
     }
 }
 
--- vsts-variable-set/vsts-variable-set.ts
+++ vsts-variable-set/vsts-variable-set.ts
@@ -5,8 +5,31 @@ const value = tl.getInput("Value");
 
 const ifVariable = tl.getInput("IfVariable");
 const ifValueRegex = tl.getInput("IfValueRegex");
+const alternativeValue = tl.getInput("AlternativeValue");
 
-if (validateIfStatement(ifVariable, ifValueRegex)) {
+const applyConstraint = tl.getInput("applyConstraint");
+const applyAlternativeValue = tl.getInput("applyAlternativeValue");
+
+if (validateIfStatement(applyConstraint, ifVariable, ifValueRegex)) {
+    applyValue(variable, value);
+}
+else {
+
+    if (applyAlternativeValue.toUpperCase() === "TRUE")
+        applyValue(variable, alternativeValue);
+}
+
+function validateIfStatement(applyConstraint, ifVar, valueRegex) {
+
+    if (applyConstraint.toUpperCase() === "FALSE") {
+        return true;
+    }
+
+    var expr = new RegExp(valueRegex);
+    return expr.test(ifVar);
+}
+
+function applyValue(variable, value) {
 
     if (variable.search(/^Build[._]BuildNumber$/i) >= 0) {
         tl.command("build.updatebuildnumber", null, value);
@@ -17,14 +40,4 @@ if (validateIfStatement(ifVariable, ifValueRegex)) {
         tl._writeLine(`Set ${variable} to: ${value}`);
         tl.setResult(tl.TaskResult.Succeeded, `Set ${variable} to: ${value}`);
     }
-}
-
-function validateIfStatement(ifVar, valueRegex) {
-
-    if (ifVar === "") {
-        return true;
-    }
-
-    var expr = new RegExp(valueRegex);
-    return expr.test(ifVar);
 }
