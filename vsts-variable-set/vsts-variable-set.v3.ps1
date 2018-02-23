017131887995daba3f8764cfcaab0f820e835401
Jesse Houwing <jesse.houwing@gmail.com>
2018-02-23 20:05:46 +0100

    Move vsts-variable-set to PS3 handler.

---
+++ vsts-variable-set/vsts-variable-set.v3.ps1
@@ -0,0 +1,18 @@
+﻿Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
+Write-Verbose "Parameter Values"
+
+$VariableName = Get-VstsInput -name "VariableName"
+$Value = Get-VstsInput -name "Value" -default ""
+
+Write-Output "Setting '$VariableName' to '$Value'."
+
+if ($VariableName -eq "Build.BuildNumber")
+{
+	Write-VstsUpdateBuildNumber -value $Value
+}
+else
+{
+	Set-VstsTaskVariable -name $VariableName -value $Value
+}
+
+Write-VstsSetResult -Result "Succeeded" -message "DONE"
