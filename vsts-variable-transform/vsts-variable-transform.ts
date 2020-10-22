2031224187cc7402023a316c010c17a67ad46c02
Jesse Houwing <jesse.houwing@gmail.com>
2020-10-22 12:23:25 +0200

    Use `updateReleaseName` to set the release name protected variable

--- vsts-variable-transform/vsts-variable-transform.ts
+++ vsts-variable-transform/vsts-variable-transform.ts
@@ -39,13 +39,22 @@ const variable = tl.getInput("variableName", true);
 
 if (variable.search(/^Build[._]BuildNumber$/i) >= 0) {
     if (useTaskLib) {
-        tl.command("build.updatebuildnumber", null, value);
+        tl.updateBuildNumber(value);
     } else {
         console.log(`##vso[build.updatebuildnumber]${value}`);
     }
-
+    
     console.log(`Set buildnumber to: ${value}`);
     tl.setResult(tl.TaskResult.Succeeded, `Set buildnumber to: ${value}`);
+} else if (variable.search(/^release[._]releasename$/i) >= 0) {
+    if (useTaskLib) {
+        tl.updateReleaseName(value);
+    } else {
+        console.log(`##vso[release.updatereleasename]${value}`);
+    }
+    
+    console.log(`Set release name to: ${value}`);
+    tl.setResult(tl.TaskResult.Succeeded, `Set release name to: ${value}`);
 } else {
     if (useTaskLib) {
         tl.setVariable(variable, value, isSecret);
