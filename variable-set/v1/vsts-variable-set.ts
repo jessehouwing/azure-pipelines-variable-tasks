750e3343a9fc0ac9763ce812885e17f2ee357b97
Jesse Houwing <jesse.houwing@gmail.com>
2020-11-03 11:44:34 +0100

    Updating to v2 for ReleaseName features
    **Warning** changes the default values on some properties
    Dumping old powershell tasks

---
+++ variable-set/v1/vsts-variable-set.ts
@@ -0,0 +1,26 @@
+﻿import * as tl from "azure-pipelines-task-lib/task";
+
+const variable = tl.getInput("VariableName", true);
+const value = tl.getInput("Value");
+const isSecret = tl.getBoolInput("isSecret") || false;
+const useTaskLib = tl.getBoolInput("useTasklib") || false;
+
+if (variable.search(/^Build[._]BuildNumber$/i) >= 0) {
+    if (useTaskLib) {
+        tl.command("build.updatebuildnumber", null, value);
+    } else {
+        console.log(`##vso[build.updatebuildnumber]${value}`);
+    }
+    
+    console.log(`Set buildnumber to: ${value}`);
+    tl.setResult(tl.TaskResult.Succeeded, `Set buildnumber to: ${value}`);
+} else {
+    if (useTaskLib) {
+        tl.setVariable(variable, value, isSecret);
+    } else {
+        console.log(`##vso[task.setvariable variable=${variable};isSecret=${ isSecret ? 'true' : 'false' };]${value}`);
+    }
+    
+    console.log(`Set ${variable} to: ${value}`);
+    tl.setResult(tl.TaskResult.Succeeded, `Set ${variable} to: ${value}`);
+}
