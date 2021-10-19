85b32e9231789b67e91ef3803777a06ec57d4402
Jesse Houwing <jesse.houwing@gmail.com>
2021-10-19 14:56:54 +0200

    ARGH!

--- variable-set/v2/vsts-variable-set.ts
+++ variable-set/v2/vsts-variable-set.ts
@@ -29,7 +29,7 @@ if (variable.search(/^Build[._]BuildNumber$/i) >= 0) {
     if (useTaskLib) {
         tl.setVariable(variable, value, isSecret);
     } else {
-        console.log(`##vso[task.setvariable variable=${variable}${ isSecret ? ';isSecret=true' : 'false' }${ isOutput ? ';isOutput=true' : ''};]${value}`);
+        console.log(`##vso[task.setvariable variable=${variable};isSecret=${ isSecret ? 'true' : 'false' };isOutput=${ isOutput ? 'true' : 'false' };]${value}`);
     }
     
     const newValue=tl.getVariable(variable);
